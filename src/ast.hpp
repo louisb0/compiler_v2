58cbbbea219a28d4855514483388015d3fb1bbdb
louisb0 <louisbaragwanath@outlook.com>
2024-10-04 18:36:22 +1300

    Switch ordering of methods on nodes

--- src/ast.hpp
+++ src/ast.hpp
@@ -1,17 +1,13 @@
 #ifndef AST_H
 #define AST_H
 
-// TODO: rework
+// TODO: rework - switch into statements and expressions, any use for namespaces?
 
 #include <memory>
 #include <string>
 #include <utility>
 
 #include "visitors/visitor.hpp"
-
-// enum class Type {
-//     I32,
-// };
 
 class Node {
 public:
@@ -21,9 +17,94 @@ public:
 };
 
 class Statement : public Node {};
+
 class Expression : public Node {};
 
-//  Statements
+// Statements
+class Print : public Statement {
+public:
+    Print(std::unique_ptr<Expression> expr) : expr(std::move(expr)) {}
+
+    const Expression *getExpression() const { return expr.get(); }
+
+    void accept(Visitor &visitor) const override { visitor.visitPrintStatement(*this); }
+
+private:
+    std::unique_ptr<Expression> expr;
+};
+
+class ExpressionStatement : public Statement {
+public:
+    ExpressionStatement(std::unique_ptr<Expression> expr) : expr(std::move(expr)) {}
+
+    const Expression *getExpression() const { return expr.get(); }
+
+    void accept(Visitor &visitor) const override { visitor.visitExpressionStatement(*this); }
+
+private:
+    std::unique_ptr<Expression> expr;
+};
+
+//  Expressions
+class Binary : public Expression {
+public:
+    Binary(std::string op, std::unique_ptr<Expression> left, std::unique_ptr<Expression> right)
+        : op(op), left(std::move(left)), right(std::move(right)) {};
+
+    const std::string &getOperator() const { return op; }
+    const Expression *getLeft() const { return left.get(); }
+    const Expression *getRight() const { return right.get(); }
+
+    void accept(Visitor &visitor) const override { visitor.visitBinaryExpression(*this); }
+
+private:
+    std::string op;
+    std::unique_ptr<Expression> left;
+    std::unique_ptr<Expression> right;
+};
+
+class Unary : public Expression {
+public:
+    Unary(std::string op, std::unique_ptr<Expression> expr) : op(op), expr(std::move(expr)) {}
+
+    const std::string &getOperator() const { return op; }
+    const Expression *getExpression() const { return expr.get(); }
+
+    void accept(Visitor &visitor) const override { visitor.visitUnaryExpression(*this); }
+
+private:
+    std::string op;
+    std::unique_ptr<Expression> expr;
+};
+
+class Grouping : public Expression {
+public:
+    Grouping(std::unique_ptr<Expression> expr) : expr(std::move(expr)) {}
+
+    const Expression *getExpression() const { return expr.get(); }
+
+    void accept(Visitor &visitor) const override { visitor.visitGroupingExpression(*this); }
+
+private:
+    std::unique_ptr<Expression> expr;
+};
+
+class Number : public Expression {
+public:
+    Number(int value) : value(value) {}
+
+    const int getValue() const { return value; }
+
+    void accept(Visitor &visitor) const override { visitor.visitNumberExpression(*this); }
+
+private:
+    int value;
+};
+
+// enum class Type {
+//     I32,
+// };
+
 // class Declaration : public Statement {
 // public:
 //     Declaration(Token name, std::unique_ptr<Expression> expr, Type type)
@@ -40,62 +121,6 @@ class Expression : public Node {};
 //     std::unique_ptr<Expression> expr;
 //     Type type;
 // };
-
-class Print : public Statement {
-public:
-    Print(std::unique_ptr<Expression> expr) : expr(std::move(expr)) {}
-
-    void accept(Visitor &visitor) const override { visitor.visitPrintStatement(*this); }
-
-    const Expression *getExpression() const { return expr.get(); }
-
-private:
-    std::unique_ptr<Expression> expr;
-};
-
-class ExpressionStatement : public Statement {
-public:
-    ExpressionStatement(std::unique_ptr<Expression> expr) : expr(std::move(expr)) {}
-
-    void accept(Visitor &visitor) const override { visitor.visitExpressionStatement(*this); }
-
-    const Expression *getExpression() const { return expr.get(); }
-
-private:
-    std::unique_ptr<Expression> expr;
-};
-
-//  Expressions
-class Binary : public Expression {
-public:
-    Binary(std::string op, std::unique_ptr<Expression> left, std::unique_ptr<Expression> right)
-        : op(op), left(std::move(left)), right(std::move(right)) {};
-
-    void accept(Visitor &visitor) const override { visitor.visitBinaryExpression(*this); }
-
-    const std::string &getOperator() const { return op; }
-    const Expression *getLeft() const { return left.get(); }
-    const Expression *getRight() const { return right.get(); }
-
-private:
-    std::string op;
-    std::unique_ptr<Expression> left;
-    std::unique_ptr<Expression> right;
-};
-
-class Unary : public Expression {
-public:
-    Unary(std::string op, std::unique_ptr<Expression> expr) : op(op), expr(std::move(expr)) {}
-
-    void accept(Visitor &visitor) const override { visitor.visitUnaryExpression(*this); }
-
-    const std::string &getOperator() const { return op; }
-    const Expression *getExpression() const { return expr.get(); }
-
-private:
-    std::string op;
-    std::unique_ptr<Expression> expr;
-};
 
 // class Assignment : public Expression {
 // public:
@@ -124,29 +149,5 @@ private:
 // private:
 //     Token name;
 // };
-
-class Grouping : public Expression {
-public:
-    Grouping(std::unique_ptr<Expression> expr) : expr(std::move(expr)) {}
-
-    void accept(Visitor &visitor) const override { visitor.visitGroupingExpression(*this); }
-
-    const Expression *getExpression() const { return expr.get(); }
-
-private:
-    std::unique_ptr<Expression> expr;
-};
-
-class Number : public Expression {
-public:
-    Number(int value) : value(value) {}
-
-    void accept(Visitor &visitor) const override { visitor.visitNumberExpression(*this); }
-
-    const int getValue() const { return value; }
-
-private:
-    int value;
-};
 
 #endif
